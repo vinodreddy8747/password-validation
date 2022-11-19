9a9ef6b0f662cff7d5832215b3db16d5e0aaf4a5
vinodreddy8747 <vinod.reddy8747@gmail.com>
2022-11-19 18:38:01 +0530

    Unit test for Password policy all rules

--- src/main/java/com/example/passwordvalidation/service/impl/PasswordValidationService.java
+++ src/main/java/com/example/passwordvalidation/service/impl/PasswordValidationService.java
@@ -1,42 +1,37 @@
 package com.example.passwordvalidation.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
-import com.example.passwordvalidation.dto.PasswordLengthRule;
-import com.example.passwordvalidation.dto.PasswordNotNullRule;
-import com.example.passwordvalidation.dto.PasswordOneLowerCaseRule;
-import com.example.passwordvalidation.dto.PasswordOneNumberRule;
-import com.example.passwordvalidation.dto.PasswordOneUpperCaseRule;
 import com.example.passwordvalidation.dto.PasswordValidationRule;
 import com.example.passwordvalidation.dto.PasswordValidationRuleResponse;
 import com.example.passwordvalidation.exception.PasswordPolicyException;
+import com.example.passwordvalidation.utils.AppConstants;
 
 @Service
 public class PasswordValidationService {
-	
-	List<PasswordValidationRule> ruleList;
-	
-	public boolean isValidPassword(String password) throws PasswordPolicyException {
-		ruleList = new ArrayList<>();
-		ruleList.add(new PasswordLengthRule());
-		ruleList.add(new PasswordNotNullRule());
-		ruleList.add(new PasswordOneUpperCaseRule());
-		ruleList.add(new PasswordOneLowerCaseRule());
-		ruleList.add(new PasswordOneNumberRule());
+
+	public boolean isValidPassword(String password, List<PasswordValidationRule> ruleList)
+			throws PasswordPolicyException {
 		
-		List<PasswordValidationRuleResponse> ruleResponse = ruleList.stream().map(obj -> obj.isValid(password)).collect(Collectors.toList());
+		if(StringUtils.isEmpty(password)) {
+			throw new PasswordPolicyException(HttpStatus.BAD_REQUEST.value(), AppConstants.PASSWORD_NOT_NULL_MSG);
+		}
+
+		List<PasswordValidationRuleResponse> ruleResponse = ruleList.stream().map(obj -> obj.isValid(password))
+				.collect(Collectors.toList());
 		List<String> errorList = ruleResponse.stream().filter(obj -> !obj.isValid())
-		.map(PasswordValidationRuleResponse::getErrorMessage).collect(Collectors.toList());
-		
-		if(CollectionUtils.isEmpty(errorList)) {
+				.map(PasswordValidationRuleResponse::getErrorMessage).collect(Collectors.toList());
+
+		if (CollectionUtils.isEmpty(errorList)) {
 			return true;
-		}else {
-			throw new PasswordPolicyException(400, errorList.toString());
+		} else {
+			throw new PasswordPolicyException(HttpStatus.BAD_REQUEST.value(), errorList.toString());
 		}
 	}
 
